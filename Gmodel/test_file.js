5ab10831996a2d4a34e8a2aa8cf4c3a366aea37e
Japsz <benjamin.meneses.14@sansano.usm.cl>
2019-03-04 18:45:12 -0300

    cambios a la api key

--- Gmodel/test_file.js
+++ Gmodel/test_file.js
@@ -10,7 +10,7 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
-fs.readFile('../credentials.json', (err, content) => {
+fs.readFile('credentials/profesores-232319-75cda3905e88.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
 // Authorize a client with credentials, then call the Google Calendar API.
 authorize(JSON.parse(content), listEvents);
